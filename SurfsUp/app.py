cad85386c1e4e6bedbb21a4c85bfb5490a251016
CJO100293 <codyjamesosieczonek@outlook.com>
2023-06-08 21:37:57 -0700

    updated '/SurfsUp/app.py'

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,58 @@
 # Import the dependencies.
-
-
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy import func
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
 
-
 # reflect an existing database into a new model
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+data = Base.classes.data
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def homepage():
+        return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation:<br/>"
+        f"/api/v1.0/stations:<br/>"
+        f"/api/v1.0/tobs:<br/>"
+        f"/api/v1.0/start/end"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+        
+@app.route("/api/v1.0/stations")
+def stations():
+        
+@app.route("/api/v1.0/tobs")
+def tobs():
+        
+@app.route("/api/v1.0/<start>/<end>")
+def start_end():
